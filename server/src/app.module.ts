ba6279d8bed50c9e2fe9dec4e92de5c0bd81d12b
Matvey Volkov <83922413+Waljunye@users.noreply.github.com>
2022-09-18 21:17:30 +0500

    CSAP-4 User Mapping

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {SequelizeModule} from "@nestjs/sequelize";
+import { UserModule } from './user/user.module';
+import {ConfigModule} from "@nestjs/config";
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+         envFilePath: '.env'
+      }),
+      SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          autoLoadModels: true
+      }),
+      UserModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    console.log(typeof process.env.DB_PASSWORD)
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
 }
 bootstrap();
